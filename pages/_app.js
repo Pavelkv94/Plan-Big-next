20ab71fc598669c28db25a6ccd147f269b5c6147
Pavel Kazlou <obilonix@proton.me>
2023-09-28 15:17:29 +0300

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
 import "../styles/global.scss";
 import { ThemeProvider } from '../components/ThemeContext';
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider>
+      <Head>
+        <meta keywords="pavel"></meta>
+        <title>Plan Big</title>
+        <link rel="shortcut icon" href="/favicon.png" type="image/x-icon"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&family=Tektur:wght@400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
--- pages/index.js
+++ pages/index.js
@@ -23,14 +23,7 @@ const Index = () => {
 
   return (
     <>
-      <Head>
-        <meta keywords="pavel"></meta>
-        <title>Plan Big</title>
-        <link rel="shortcut icon" href="/favicon.png" type="image/x-icon"/>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&family=Tektur:wght@400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
-      </Head>
+      
       <div className="app-wrapper">
         <HeaderNavMenu isDarkTheme={isDarkTheme} setOpenModal={setOpenModal}/>
         <Main isDarkTheme={isDarkTheme} toggleTheme={toggleTheme} />
