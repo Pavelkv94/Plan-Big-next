3f2a078c2e8e51079e61ad9060858a20d365f4cb
Pavel Kazlou <obilonix@proton.me>
2023-10-31 17:15:43 +0300

    fix

--- components/Main.js
+++ components/Main.js
@@ -15,12 +15,12 @@ const Main = ({ isDarkTheme, toggleTheme, screenSize }) => {
     <div className={`${styles.mainWrapper} ${isDarkTheme ? styles.dark : styles.light}`} id="main">
       <div className={styles.mainContent}>
         <div className={styles.themeSwitcherWrapper}>
-          <div className={`${styles.themeSwitcher} ${isDarkTheme ? styles.dark : styles.light}`} onClick={switchTheme}>
+        <a href="https://main--inspiring-starburst-70d656.netlify.app"><div className={`${styles.themeSwitcher} ${isDarkTheme ? styles.dark : styles.light}`}>
             <div className={styles.pulse}></div>
             <div  className={styles.toggler} style={{ transform: `translateX(${position}%)`, transition: "transform 0.3s ease" }}>
               <div className={styles.togglerBox}></div>
             </div>
-          </div>
+          </div></a>
         </div>
 
         <div className={styles.mainInfo}>
